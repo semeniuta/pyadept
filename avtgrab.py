6fef28cd3ee0f3d3799794623e52667dd42ecc9b
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2016-02-19 16:05:43 +0100

    Fix in avtgrab; created save4offline

--- avtgrab.py
+++ avtgrab.py
@@ -9,15 +9,33 @@ class AVTImageGrabber:
 
     def grab_image(self):
         self.last_scv_im = self.cam.getImage()  
-        q.put(self.last_scv_im.getNumpy())
+        self.q.put(self.last_scv_im.getNumpyCv2())
         
+    def __del__(self):
+        self.cam.setupSyncMode()
+
         
 if __name__ == '__main__':
 
     from Queue import Queue
     from flexvi.core.images import save_image
     
-    q = Queue()
-    g = AVTImageGrabber(3, q)
+    g1 = AVTImageGrabber(0, Queue())
+    g2 = AVTImageGrabber(1, Queue())
+    
+    #p1_start = g1.cam.getAllProperties()
+    
+    g1.grab_image()
+    g2.grab_image()
+    
+    save_image(g1.q.get(), 'im1.png')
+    save_image(g2.q.get(), 'im2.png')
+    
+    #p1_end = g1.cam.getAllProperties()
+    
+    #for k, v0 in p1_start.iteritems():
+    #    v1 = p1_end[k]
+    #    if v0 != v1:
+    #        print k, v0, v1  
     
     
