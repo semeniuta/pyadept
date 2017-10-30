513767d9344a3c9beda375bc47b87d4bcf87ccce
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2017-10-30 20:08:05 +0100

    Added testclient2.py (Python 3, with Adept controller)

--- adeptclient.py
+++ adeptclient.py
@@ -10,8 +10,8 @@ class AdeptClient:
             print 'Client socket created'
         except:
             print 'Failed to create a socket'
-            sys.exit()    
-        
+            sys.exit()
+
     def connect_to_server(self, host, port):
         try:
             dest_pair = (host, port)
@@ -21,27 +21,27 @@ class AdeptClient:
         except:
             print 'Failed to connect'
             sys.exit()
-        
+
     def send_msg(self, msg):
         try:
             self.s.sendall(msg)
             t_send = time.time() - self.t_conn
             print '[%.3f]\tsend\t%s' % (t_send, msg)
-            
+
         except:
             print 'Failed to send data'
             sys.exit()
-        
-        try: 
+
+        try:
             resp = self.s.recv(2048)
             t_recv = time.time() - self.t_conn
             print '[%.3f]\trecv\t%s' % (t_recv, resp.strip())
         except:
             print 'Failed to receive data'
             sys.exit()
-        
-    def close(self):    
+
+    def close(self):
         self.s.close()
-        
+
     def __destr__(self):
         self.close()
